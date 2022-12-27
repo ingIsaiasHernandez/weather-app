96cd4f1220449f39053913c36d0599c94cadebec
ingIsaiasHernandez <ing.isaias.hdez@gmail.com>
2022-12-27 16:07:39 -0600

    [FIX] - The icon of APi was fixed and the style was update

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,6 +1,6 @@
 import axios, { formToJSON } from 'axios';
 import React from 'react';
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react'
 
 const Weather = () => {
 
@@ -8,9 +8,29 @@ const Weather = () => {
 const [isweather, setIsWeather] = useState([]);
 
 useEffect(() => {
+
+
+    if('geolocation' in navigator){
+
+        console.log('geolocation available')
     
-    axios.get('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=8b5fb68fdc148d5c789bfda264214869&units=metric')
-        .then(response => setIsWeather(response.data))
+        navigator.geolocation.getCurrentPosition((position) => {
+            const lat = position.coords.latitude;
+            const lon = position.coords.longitude;
+
+            console.log(lat, lon);
+
+
+            axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=8b5fb68fdc148d5c789bfda264214869&units=metric`)
+                .then(response => setIsWeather(response.data))
+        });
+
+    
+    }else{
+        console.log('geolocation not available')
+    }
+    
+    
 },[]);
 
 
@@ -31,21 +51,23 @@ const changetemperature = () => {
 
     return (
         <div className='container'>
+
+            <h2 className="card-title">Weather app</h2>
+
             <div className="card-content">
                    
 
                 <div className="card-content--info">
-                    <h1 className="card-title">Weather app</h1>
-                    <h2 className="card-subtitle">{isweather.name}</h2>
+                    <h2 className="card-subtitle">{isweather.name}, {isweather.sys?.country}</h2>
                     <h2 className="card-subtitle">Wind speed: {isweather.wind?.speed} m/s</h2>
                     <h2 className="card-subtitle">Clouds: {isweather.clouds?.all}%</h2>
-                    <h2 className="card-subtitle">Pressure: {isweather.main?.pressure}</h2>
+                    <h2 className="card-subtitle">Pressure: {isweather.main?.pressure} mb</h2>
                 </div>
 
                 <div className="card-content--img">
-                <h2 className="card-subtitle">{isweather.weather?.[0]?.description} </h2>
+                <h2 className="card-subtitle">"{isweather.weather?.[0]?.description}" </h2>
 
-                    <img src={isweather.weather?.main} alt="" className="card-img" />
+                    <img src={`http://openweathermap.org/img/wn/${isweather.weather?.[0].icon}.png`} alt="weather-icon" className="card-img" />
                     <h2 className="card-subtitle--degrees">
                        {ismetric ? inCelsius : inFahrenheit}
                        {' '}
@@ -53,10 +75,8 @@ const changetemperature = () => {
                     </h2>
                 </div>
                 
-                <button className="card-btn" onClick={changetemperature}>Degrees °F/°C</button>
-                
             </div>
-            
+            <button className="card-btn" onClick={changetemperature}>Degrees °F/°C</button>
         </div>
     );
 };
